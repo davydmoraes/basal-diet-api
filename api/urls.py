f21284a4d567cea0275abf62d7cea1fa8df6fd7a
Davyd Moraes <dev.davydmoraes@gmail.com>
2024-01-28 16:37:15 -0300

    update on crud architecture

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,29 @@
 from django.urls import path
-from .views import FoodList, FoodDetail, UserList, UserDetail, MealList, MealDetail, MealFoodList, MealFoodDetail
+from . import views
 
 urlpatterns = [
-    path('food/', FoodList.as_view()),
-    path('food/<int:pk>/', FoodDetail.as_view()),
-    path('user/', UserList.as_view()),
-    path('user/<int:pk>', UserDetail.as_view()),
-    path('meal/', MealList.as_view()),
-    path('meal/<int:pk>', MealDetail.as_view()),
-    path('meal-food/', MealFoodList.as_view()),
-    path('meal-food/<int:pk>', MealFoodDetail.as_view()),
-
+    path('', views.apiOverview, name="api-overview"),
+    
+    path('food-list/', views.foodList, name="food-list"),
+    path('user-list/', views.userList , name="user-list"),
+    path('meal-list/', views.mealList, name="meal-list"),
+    path('meal-food-list/', views.mealFoodList, name="meal-food-list"),
+    
+    path('food-detail/<int:pk>/', views.foodDetail, name="food-detail"),
+    path('user-detail/<int:pk>/', views.userDetail, name="user-detail"),
+    path('meal-detail/<int:pk>/', views.mealDetail, name="meal-detail"),
+    
+    path('create-food/', views.createFood, name="create-food"),
+    path('create-user/', views.createUser, name="create-user"),
+    path('create-meal/', views.createMeal, name="create-meal"),
+    path('create-meal-food/', views.createMealFood, name="create-meal-food"),
+   
+   
+    path('update-food/<int:pk>/', views.updateFood, name="update-food"),
+    path('update-user/<int:pk>/', views.updateUser, name="update-user"),
+    path('update-meal/<int:pk>/', views.updateMeal, name="update-meal"),
+    
+    path('delete-food/<int:pk>/', views.deleteFood, name="delete-food"),
+    path('delete-user/<int:pk>/', views.deleteUser, name="delete-user"),
+    path('delete-meal/<int:pk>/', views.deleteMeal, name="delete-meal"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,47 +1,173 @@
 from rest_framework import generics
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .models import Food, User, Meal, MealFood
 from .serializers import FoodSerializer, UserSerializer, MealSerializer, MealFoodSerializer
 
 
-'''class FoodList(generics.ListCreateAPIView):
-    serializer_class = FoodSerializer
+################  CRUD OVERVIEW  ################
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'Create Food':'/api/create-food/',
+        'Food List':'/api/food-list',
+        'Food Details':'/api/food-detail/<int:pk>',
+        'Update Food':'/api/update-food/<int:pk>',
+        'Delete Food':'/api/delete-food/<int:pk>',   
 
-    def get_queryset(self):
-        queryset = Food.objects.all()
-        user = self.request.query_param.get('user')
-        if user is not None:
-            queryset = queryset.filter(foodUser=user)
-        return queryset'''
-    
-class FoodList(generics.ListCreateAPIView):
-    serializer_class = FoodSerializer
-    queryset = Food.objects.all()    
-
-class UserList(generics.ListCreateAPIView):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
-    
-class FoodDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = FoodSerializer
-    queryset = Food.objects.all()
-    
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
-    
-class MealList(generics.ListCreateAPIView):
-    serializer_class = MealSerializer
-    queryset = Meal.objects.all()
-    
-class MealDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = MealSerializer
-    queryset = Meal.objects.all()
+        'Create User':'/api/create-user/',
+        'User List':'/api/user-list',
+        'User Details':'/api/user-detail/<int:pk>',
+        'Update User':'/api/update-user/<int:pk>',
+        'Delete User':'/api/delete-user/<int:pk>',
         
-class MealFoodList(generics.ListCreateAPIView):
-    serializer_class = MealFoodSerializer
-    queryset = MealFood.objects.all()
-    
-class MealFoodDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = MealFoodSerializer
-    queryset = MealFood.objects.all()
+        'Create Meal':'/api/create-meal/',
+        'Meal List':'/api/meal-list',
+        'Meal Details':'/api/meal-detail/<int:pk>',
+        'Update Meal':'/api/update-meal/<int:pk>',
+        'Delete Meal':'/api/delete-meal/<int:pk>',
+        
+        'Create MealFood':'/api/create-meal-food/',
+        'MealFood List':'/api/meal-food-list',
+        
+    }
+    return Response(api_urls)
+
+
+
+################  FOOD CRUD  ################
+@api_view(['POST'])
+def createFood(request):
+    serializer = FoodSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+                
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def foodList(request):
+    food = Food.objects.all()
+    serializer = FoodSerializer(food, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def foodDetail(request, pk):
+    food = Food.objects.get(id=pk)
+    serializer = FoodSerializer(food, many=False)
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteFood(request, pk):
+    food = Food.objects.get(id=pk)
+    food.delete()
+                
+    return Response('Item successfully deleted')
+
+@api_view(['POST'])
+def updateFood(request, pk):
+    food = Food.objects.get(id=pk)
+    serializer = FoodSerializer(instance=food, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+                
+    return Response(serializer.data)
+
+
+
+################  USER CRUD  ################
+@api_view(['POST'])
+def createUser(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+                
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def userList(request):
+    user = User.objects.all()
+    serializer = UserSerializer(user, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def userDetail(request, pk):
+    user = User.objects.get(id=pk)
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def updateUser(request, pk):
+    user = User.objects.get(id=pk)
+    serializer = UserSerializer(instance=user, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+                
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteUser(request, pk):
+    user = User.objects.get(id=pk)
+    user.delete()
+                
+    return Response('Item successfully deleted')
+
+
+
+
+################  MEAL CRUD  ################
+@api_view(['POST'])
+def createMeal(request):
+    serializer = MealSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+                
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def mealList(request):
+    meal = Meal.objects.all()
+    serializer = MealSerializer(meal, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def mealDetail(request, pk):
+    meal = Meal.objects.get(id=pk)
+    serializer = MealSerializer(meal, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def updateMeal(request, pk):
+    meal = Meal.objects.get(id=pk)
+    serializer = MealSerializer(instance=meal, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+                
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteMeal(request, pk):
+    meal = Meal.objects.get(id=pk)
+    meal.delete()
+                
+    return Response('Item successfully deleted')
+
+
+
+
+################  MEAL-FOOD  ################
+@api_view(['GET'])
+def mealFoodList(request):
+    mealFood = MealFood.objects.all()
+    serializer = MealFoodSerializer(mealFood, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def createMealFood(request):
+    serializer = MealFoodSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+                
+    return Response(serializer.data)
